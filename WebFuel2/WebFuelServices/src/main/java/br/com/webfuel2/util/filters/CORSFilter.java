fc8cc56d3233b87d490d106bfdef0281f7c9d7ba
fsilvapucci@gmail.com <fsilvapucci@gmail.com>
2016-07-01 10:33:55 -0300

    Adição dos filtros para prevenção de problemas CORS
    de acordo com:
    https://felipenipo.wordpress.com/2015/04/10/implementando-cors-em-web-services-com-jax-rs/#more-275

---
+++ WebFuel2/WebFuelServices/src/main/java/br/com/webfuel2/util/filters/CORSFilter.java
@@ -0,0 +1,31 @@
+package br.com.webfuel2.util.filters;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public ContainerResponse filter(ContainerRequest containerRequest,
+	    ContainerResponse containerResponse) {
+	ResponseBuilder responseBuilder = Response
+                .fromResponse(containerResponse.getResponse());
+
+	responseBuilder.header("Access-Control-Allow-Origin", "*");
+	responseBuilder.header("Access-Control-Allow-Methods", 
+                "POST, GET, OPTIONS");
+	responseBuilder.header("Access-Control-Allow-Headers", 
+                "Foo-Header");
+        responseBuilder.header("Access-Control-Max-Age",
+                "86400");
+
+	containerResponse.setResponse(responseBuilder.build());
+
+	return containerResponse;
+    }
+
+}
